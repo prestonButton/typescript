688d0f370b80b0a2aa1fa40f3489b9d580c693aa
Preston Button <prestonbutton99@gmail.com>
2024-11-04 10:08:15 +0200

    initial commit

---
+++ typescript.ts
@@ -0,0 +1,37 @@
+// Lesson 1: Introduction to Variables
+
+// Variables are used to store data that can be used and manipulated in a program.
+// In TypeScript, we can declare variables using `let`, `const`, or `var`.
+
+// `let` is used to declare a variable that can be reassigned.
+
+// `const` is used to declare a variable that cannot be reassigned.
+
+// `var` is an older way to declare variables, but it's not recommended due to its function-scoped behavior.
+
+// Lesson 2: Data Types
+
+// TypeScript is a statically typed language, which means we need to specify the type of data a variable can hold.
+
+// Number: Used to represent numeric values.
+
+// String: Used to represent text.
+
+// Boolean: Used to represent true or false values.
+
+// Null: Represents the intentional absence of any object value.
+
+// Undefined: Represents an uninitialized variable.
+
+// Array: Used to store multiple values in a single variable.
+
+// Object: Used to store a collection of key-value pairs.
+
+// Tuple: Used to store an array with a fixed number of elements of different types.
+
+// Enum: Used to define a set of named constants.
+
+// Any: Used to store any type of value. Use with caution as it can defeat the purpose of TypeScript's type safety.
+
+// Void: Used to represent the absence of any type, commonly used as the return type of functions that do not return a value.
+
